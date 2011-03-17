9c66eedce3ee24daaf2e9ee211bb5eab46bf8926
Masakuni Kato <mackato@gmail.com>
2011-03-18 00:36:41 +0900

    リクエストハンドラをclientを使った実装に書き換えました。バージョンを5までインクリメントしました。

--- app/handlers.py
+++ app/handlers.py
@@ -3,7 +3,7 @@
 from google.appengine.ext import webapp
 from google.appengine.api import taskqueue, memcache
 
-import tweepy, consumer, token, logging, re
+import client, logging
 
 class CronHandler(webapp.RequestHandler):
   def get(self):
@@ -20,36 +20,33 @@ class UpdateHandler(webapp.RequestHandler):
     POST /update
     """
     try:
-      auth = tweepy.OAuthHandler(consumer.KEY, consumer.SECRET)
-      auth.set_access_token(token.KEY, token.SECRET)
-      self.api = tweepy.API(auth)
+      self.client = client.Client()
       self.execute()
       self.response.out.write("Success!")
     except Exception, e:
-      logging.error(e)
       self.response.out.write("Error!")
   
   def execute(self):
     """
     Execute status update or subscribe user by mentions that tweeted after last update.
     """
-    status_id = None
+    status = None
     try:
-      mentions = self.api.mentions()
+      # mentions = self.api.mentions()
       last_update_id = self.last_update_id()
-      mentions.reverse()
-      for status in mentions:
-        if not (last_update_id and status.id <= last_update_id):
-          if status.text.strip() == u'@hamamatsurb subscribe':
-            status_id = self.subscribe_user(status)
-          else:
-            status_id = self.update_status(status)
-      if status_id:
-        memcache.set('last_update_id', status_id)
+      messages = self.client.get_last_messages(last_update_id)
+      for message in messages:
+        if self.client.is_subscribe_message(message):
+          status = self.client.subscribe_user(message.user)
+        else:
+          text = self.client.convert_message(message)
+          status = self.client.update_status(text)
+        memcache.set('last_update_id', status.id)
     except Exception, e:
-      if status_id:
-        memcache.set('last_update_id', status_id)
-      logging.error(e)
+      if status:
+        memcache.set('last_update_id', status.id)
+      logging.error("UpdateHandler#execute: %s" % e)
+      raise e
       
   def last_update_id(self):
     """
@@ -59,46 +56,7 @@ class UpdateHandler(webapp.RequestHandler):
     if last_update_id is not None:
       return last_update_id
     try:
-      statuses = self.api.user_timeline()
-      p = re.compile(r'hamamatsu', re.IGNORECASE)
-      for status in  statuses:
-        if p.match(status.source):
-          return status.id
-      return None
+      return self.client.get_last_update_id('hamamatsurb-bot')
     except Exception, e:
-      raise e
-    
-  def update_status(self, status):
-    """
-    Update status with a hashtag and user screen name.
-    """
-    if status.source.startswith('GitHub'):
-      return None
-    text = status.text.replace('@hamamatsurb ', '')
-    tag = '#hamamatsurb' if not re.search(r'#hamamatsurb', text) else ''
-    text = "%s %s - @%s" % (text, tag, status.user.screen_name)
-    if re.match('@', text):
-      text = ".%s" % text
-    if 140 < len(text):
-      text = "%s..." % text[0:140-3]
-    try:
-      self.api.update_status(text)
-      return status.id
-    except Exception, e:
-      raise e
-
-  def subscribe_user(self, status):
-    """
-    Follow user and add user to list.
-    """
-    screen_name = status.user.screen_name
-    try:
-      if not self.api.exists_friendship('hamamatsurb', screen_name):
-        self.api.create_friendship(screen_name)
-        self.api.add_list_member('hamamatsu-rb', status.user.id)
-        text = u".@%s さんがHamamatsu.rbに参加しました！" % screen_name
-        self.api.update_status(text)
-        logging.info(text)
-        return status.id
-    except Exception, e:
+      logging.error("UpdateHandler#last_update_id: %s" % e)
       raise e
